4614befb080dab64737bda0b84f9a1f0320d0a0c
Yashaswi <yashaswiraj050@gmail.com>
2025-08-30 12:16:44 +0530

    create service to incorporate business logic

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -1,4 +1,39 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.ToDo;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class TodoService {
+
+    private List<ToDo> todoList;
+
+    public TodoService() {
+        this.todoList = new ArrayList<>();
+    }
+
+    public String save(ToDo toDo) {
+        try {
+            if(toDo.getTitle().isBlank()){
+                String title = toDo.getTitle();
+            }
+        } catch (Exception e) {
+            return "Invalid Entry. Please provide title." + e.getMessage();
+        }
+        try{
+            for(ToDo existing: todoList){
+                if(existing.getTitle().equalsIgnoreCase(toDo.getTitle())){
+                    return "Title already exists";
+                }
+            }
+            todoList.add(toDo);
+            return "Saved Successfully";
+        }
+        catch(Exception e) {
+            return "Failed due to error, " + e.getMessage();
+        }
+    }
 }
